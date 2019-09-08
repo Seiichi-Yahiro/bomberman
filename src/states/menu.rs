f4d3c50d80cc419de0d5a2fd4af0d2327482ccc4
Julian Viechter <julian@veranstaltungstechnik-viechter.de>
2019-09-08 19:59:43 +0200

    Move font loading to load state

--- src/states/menu.rs
+++ src/states/menu.rs
@@ -4,12 +4,13 @@ use amethyst::{
     ecs::Entity,
     input::{is_key_down, VirtualKeyCode},
     prelude::*,
-    ui::{Anchor, TtfFormat, UiText, UiTransform},
+    ui::{Anchor, FontHandle, TtfFormat, UiText, UiTransform},
 };
 
 pub struct LoadMenu {
     progress_counter: ProgressCounter,
     arenas_handle: Option<Handle<Arenas>>,
+    font_handle: Option<FontHandle>,
 }
 
 impl LoadMenu {
@@ -17,6 +18,7 @@ impl LoadMenu {
         LoadMenu {
             progress_counter: ProgressCounter::new(),
             arenas_handle: None,
+            font_handle: None,
         }
     }
 }
@@ -25,20 +27,29 @@ impl SimpleState for LoadMenu {
     fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
         let world = data.world;
         let loader = world.read_resource::<Loader>();
-        let storage = world.read_resource::<AssetStorage<Arenas>>();
-        let handle = loader.load(
+
+        let arenas_handle = loader.load(
             "arenas/arenas.ron",
             RonFormat,
             &mut self.progress_counter,
-            &storage,
+            &world.read_resource(),
         );
-        self.arenas_handle = Some(handle);
+        self.arenas_handle = Some(arenas_handle);
+
+        let font_handle = world.read_resource::<Loader>().load(
+            "fonts/verdana.ttf",
+            TtfFormat,
+            &mut self.progress_counter,
+            &world.read_resource(),
+        );
+        self.font_handle = Some(font_handle);
     }
 
     fn update(&mut self, _data: &mut StateData<'_, GameData<'_, '_>>) -> SimpleTrans {
         if self.progress_counter.is_complete() {
             Trans::Switch(Box::new(Menu::new(
-                (*self.arenas_handle.as_ref().unwrap()).clone(),
+                self.arenas_handle.take().unwrap(),
+                self.font_handle.take().unwrap(),
             )))
         } else {
             Trans::None
@@ -47,6 +58,7 @@ impl SimpleState for LoadMenu {
 }
 
 pub struct Menu {
+    font_handle: FontHandle,
     arenas_handle: Handle<Arenas>,
     arenas_entities: Vec<Entity>,
     selected_arena: usize,
@@ -98,9 +110,10 @@ impl SimpleState for Menu {
 }
 
 impl Menu {
-    pub fn new(arenas_handle: Handle<Arenas>) -> Menu {
+    pub fn new(arenas_handle: Handle<Arenas>, font_handle: FontHandle) -> Menu {
         Menu {
             arenas_handle,
+            font_handle,
             arenas_entities: vec![],
             font_size: 30.0,
             selected_arena: 0,
@@ -108,13 +121,6 @@ impl Menu {
     }
 
     fn initialize_arena_selection(&self, world: &mut World) -> Vec<Entity> {
-        let font = world.read_resource::<Loader>().load(
-            "fonts/verdana.ttf",
-            TtfFormat,
-            (),
-            &world.read_resource(),
-        );
-
         let arena_names: Vec<String> = {
             let arenas_assets = world.read_resource::<AssetStorage<Arenas>>();
             arenas_assets
@@ -131,7 +137,7 @@ impl Menu {
             .enumerate()
             .map(|(index, name)| {
                 let arena_name_text = UiText::new(
-                    font.clone(),
+                    self.font_handle.clone(),
                     name.clone(),
                     [0.8, 0.8, 0.8, 1.0],
                     self.font_size,
